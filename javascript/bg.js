3b66e141953427edea8936b06c1f94581496d663
Taewook Nam <taewook122@gmail.com>
2020-08-17 16:36:51 +0900

    background image 기능 추가 :art:

--- javascript/bg.js
+++ javascript/bg.js
@@ -1,12 +1,22 @@
 const body = document.querySelector("body");
 
+const IMG_NUM = 9;
+
 function genRandom() {
-    const number = 0;
+    const number = Math.floor(Math.random() * IMG_NUM);
     return number;
+}
+
+function paintImage(imgNumber) {
+    const image = new Image;
+    image.src = `./img/${imgNumber + 1}.jpg`;
+    image.classList.add('bgImage');
+    body.appendChild(image);
 }
 
 function init() {
     const randomNumber = genRandom();
+    paintImage(randomNumber);
 }
 
 init();
