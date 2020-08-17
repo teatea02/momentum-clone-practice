bee7b20ba5eb4118b1b826de741dbfa8c4ff9469
Taewook Nam <taewook122@gmail.com>
2020-08-17 14:54:03 +0900

    todo list delete 기능 추가 :pencil2:

--- javascript/todo.js
+++ javascript/todo.js
@@ -4,16 +4,30 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
-const toDos = [];
+let toDos = [];
 
 function saveToDos() {
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function deleteToDo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo) {
+        //return toDo.id != li.id;
+        return toDo.id !== parseInt(li.id);
+    })
+    //console.log(cleanToDos);
+    toDos = cleanToDos;
+    saveToDos();
 }
 
 function paintToDo(text) {
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
     delBtn.innerText = "❌";
+    delBtn.addEventListener("click", deleteToDo);
     const span = document.createElement("span");
     const newId = toDos.length + 1;
     span.innerText = text
