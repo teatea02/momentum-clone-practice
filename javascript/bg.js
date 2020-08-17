e199889de168fee73e5301f93b4d6cea01558588
Taewook Nam <taewook122@gmail.com>
2020-08-17 14:56:34 +0900

    bg.js 스크립트 추가 :bulb:

---
+++ javascript/bg.js
@@ -0,0 +1,12 @@
+const body = document.querySelector("body");
+
+function genRandom() {
+    const number = 0;
+    return number;
+}
+
+function init() {
+    const randomNumber = genRandom();
+}
+
+init();
